ddd8fb63746d06a92ecffa22a6f1ad8cf84b8904
Kirsten Anderson <anders.kirsten@gmail.com>
2017-02-19 15:29:15 -0600

    Assignment 9 DOM Animation: Complete

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,17 +1,9 @@
-// this function should replace the for loop in the animatePoints function
+function forEach(array, callback) {
 
-function forEach() {
     // use a loop to go through all the elements in the pointsArray
-    for (var i = 0; i < points.length; i++) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    for (var i = 0; i < array.length; i++) {
+
+        // execute a callback function for each element
+        callback(array[i]);
     }
-
-    // execute a callback for each element
-    revealPoint(i);
 }
-
-
-// confirm that all selling-points are still animating properly
