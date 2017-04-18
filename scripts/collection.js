8c1e9e7a7ce0b90794d4d82c86dfd71411be2062
Kirsten Anderson <anders.kirsten@gmail.com>
2017-04-18 13:06:51 -0500

    Update Missed Style Details

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,6 +1,6 @@
 var buildCollectionItemTemplate = function() {
     var template =
-    '<div class="collection-album-container column third">'
+    '<div class="collection-album-container column fourth">'
   + '    <img src="assets/images/album_covers/01.png"/>'
   + '    <div class="collection-album-info caption">'
   + '      <p>'
